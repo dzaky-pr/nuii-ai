ee02823e194439ab43726e6ba0b2236787712ee7
dzaky-pr <dzaky.pr.mulyono97@gmail.com>
2025-04-27 01:28:59 +0700

    feat: change report page from lihat detail to download

--- app/dashboard/report/page.tsx
+++ app/dashboard/report/page.tsx
@@ -1,9 +1,12 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
+import { baseURL } from '@/lib/tools/api'
+import { useAuth } from '@clerk/nextjs'
 import { format } from 'date-fns'
 import { id } from 'date-fns/locale'
-import Link from 'next/link'
 import { useEffect, useState } from 'react'
+import { toast } from 'sonner'
 import { useGetSurveyReportList } from './_hooks/useGetSurveyReportList'
 
 const tableHeader = [
@@ -17,6 +20,11 @@ const tableHeader = [
 
 export default function ReportPage() {
   const [isMounted, setIsMounted] = useState<boolean>(false)
+  const [isDownloadLoading, setIsDownloadLoading] = useState<string | null>(
+    null
+  )
+
+  const { getToken } = useAuth()
 
   const { data: surveys, isPending: loadingGetSurveys } =
     useGetSurveyReportList()
@@ -27,6 +35,53 @@ export default function ReportPage() {
 
   if (!isMounted) {
     return null
+  }
+
+  const handleDownloadReport = async (id: string, nama_survey: string) => {
+    setIsDownloadLoading(id) // <- set id survey yg lagi loading
+    try {
+      const token = await getToken()
+
+      const response = await fetch(`${baseURL}/survey/export/excel/${id}`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept:
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        }
+      })
+
+      if (!response.ok) {
+        throw new Error(`Gagal mengunduh file report: ${response.statusText}`)
+      }
+
+      const blob = await response.blob()
+      const blobUrl = URL.createObjectURL(blob)
+
+      const date = new Date()
+      const a = document.createElement('a')
+      a.href = blobUrl
+      a.setAttribute(
+        'download',
+        `${date.getDate()}-${
+          date.getMonth() + 1
+        }-${date.getFullYear()}_REPORT-SURVEY-${nama_survey}-${id}.xlsx`
+      )
+
+      document.body.appendChild(a)
+      a.click()
+
+      window.URL.revokeObjectURL(blobUrl)
+      document.body.removeChild(a)
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error('Gagal mengunduh: ', error.message)
+        toast.error(`Gagal mengunduh file report. Silakan coba lagi!`)
+      }
+    } finally {
+      toast.success('Berhasil mengunduh file report!')
+      setIsDownloadLoading(null) // <- reset loading
+    }
   }
 
   return (
@@ -71,14 +126,29 @@ export default function ReportPage() {
                     </td>
                     <td className="border p-2">{data.user_id}</td>
                     <td className="border p-2">
-                      <Link href={`/dashboard/report/${data.id}`}>
+                      {/* <Link href={`/dashboard/report/${data.id}`}>
                         <button
                           className="text-green-500 disabled:text-neutral-500"
                           disabled={data.status_survey === 'Belum_Disetujui'}
                         >
                           Lihat Detail
                         </button>
-                      </Link>
+                      </Link> */}
+                      <Button
+                        size="sm"
+                        variant="outline"
+                        disabled={isDownloadLoading !== null}
+                        onClick={() =>
+                          handleDownloadReport(
+                            data.id.toString(),
+                            data.nama_survey
+                          )
+                        }
+                      >
+                        {isDownloadLoading === data.id.toString()
+                          ? 'Downloading...'
+                          : 'Download Report'}
+                      </Button>
                     </td>
                   </tr>
                 ))
